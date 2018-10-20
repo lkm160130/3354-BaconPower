ba78aa126e4b53cb62ddb92a1050997748500ed0
lkm160130 <38541290+lkm160130@users.noreply.github.com>
2018-10-20 18:31:48 -0500

    Made Login Activity (incomplete)

---
+++ app/src/main/java/com/example/logan/github_test/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.logan.github_test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
