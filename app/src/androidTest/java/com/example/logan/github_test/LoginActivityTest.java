9000a891d682a397307be9e4017ea069b27a1011
myh160030 <38478405+myh160030@users.noreply.github.com>
2018-12-05 18:38:55 -0600

    Finished LoginActivityTest

--- app/src/androidTest/java/com/example/logan/github_test/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/logan/github_test/LoginActivityTest.java
@@ -1,14 +1,30 @@
 package com.example.logan.github_test;
 
+import android.support.test.rule.ActivityTestRule;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
 
 public class LoginActivityTest {
 
+    //use the below commented code to access the activity for unit testing
+    @Rule
+    public ActivityTestRule<LoginActivity> nActivityRule =
+            new ActivityTestRule(LoginActivity.class);
+
+    LoginActivity loginActivity;
+    ArrayList<Song> songs;
+
     @Before
     public void setUp() throws Exception {
-
+      loginActivity = nActivityRule.getActivity();
+      // loginActivity.getBaseContext();
     }
 
     @After
@@ -16,8 +32,13 @@ public class LoginActivityTest {
     }
 
     @Test
-    public void getLoginActivity() {
+    public void checkNullUserName() {
+        assertNotNull(loginActivity.usernameEditText);
+    }
 
+    @Test
+    public void checkNullPassword() {
+        assertNotNull(loginActivity.passwordEditText);
     }
 
 }
