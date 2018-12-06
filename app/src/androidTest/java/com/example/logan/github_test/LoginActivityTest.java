e33a2233fc51f9b27fba660cbf300516c18229db
myh160030 <38478405+myh160030@users.noreply.github.com>
2018-12-05 18:07:13 -0600

    Created LoginActivityTest

---
+++ app/src/androidTest/java/com/example/logan/github_test/LoginActivityTest.java
@@ -0,0 +1,23 @@
+package com.example.logan.github_test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getLoginActivity() {
+
+    }
+
+}
